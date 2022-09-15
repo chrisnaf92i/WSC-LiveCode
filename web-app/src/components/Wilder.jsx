05daed6068a4f0f96abfd123c66abf261a3aaca4
christopher nafrere <nafrere.christopher@gmail.com>
2022-09-15 15:14:45 +0200

    Set Call API in Home components

--- web-app/src/components/Wilder.jsx
+++ web-app/src/components/Wilder.jsx
@@ -2,19 +2,26 @@ import React from 'react'
 import blank_profile from "../media/blankProfilePicture.png"
 import Skill from './Skill'
 
-export default function Wilder({firstName, lastName, skills}) {
+export default function Wilder({firstName, lastName, skills, isTrainer}) {
   return (
 
-    <article className="card">
+    <article className="card" style={isTrainer ? {border:"3px solid orange"} : {} }>
+        {
+            isTrainer? <h1 className="trainerBadge"> Trainer </h1>:<></>
+        }
         <img src={blank_profile} alt="Jane Doe Profile" />
-    <h3> {firstName} {lastName} </h3>
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-        enim ad minim veniam, quis nostrud exercitation ullamco laboris
-        nisi ut aliquip ex ea commodo consequat.
-    </p>
-    <h4>Wild Skills</h4>
+        
+        <h3> {firstName} {lastName} </h3>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+            enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat.
+        </p>
+
+        <h4>Wild Skills</h4>
+
         <ul className="skills">
             {
                 skills.map((skill) => {
--- web-app/src/pages/Home.jsx
+++ web-app/src/pages/Home.jsx
@@ -1,104 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Wilder from '../components/Wilder'
 
+
 export default function Home() {
-    const wilders = [
-        {
-            id:1,
-            firstName:"Ryota",
-            lastName:"wild",
-            skills:[
-            {
-                id:1,
-                skillName:"PHP"
-            },
-            {
-                id:2,
-                skillName:"javascript"
-            },
-            {
-                id:3,
-                skillName:"react"
-            },
-            
-            ]
-        },
-        {
-            id:2,
-            firstName:"Alan",
-            lastName:"wild",
-            skills:[
-            {
-                id:2,
-                skillName:"javascript"
-            }
-            ]
-        },
-        {
-            id:3,
-            firstName:"Hayato",
-            lastName:"wild",
-            skills:[
-            {
-                id:2,
-                skillName:"javascript"
-            }
-            ]
-        },
-        {
-            id:4,
-            firstName:"Leo",
-            lastName:"wild",
-            skills:[
-            {
-                id:2,
-                skillName:"javascript"
-            }
-            ]
-        },
-        {
-            id:5,
-            firstName:"Ryuto",
-            lastName:"wild",
-            skills:[
-            {
-                id:2,
-                skillName:"javascript"
-            }
-            ]
-        },
-        {
-            id:6,
-            firstName:"Mandy",
-            lastName:"wild",
-            skills:[
-            {
-                id:2,
-                skillName:"javascript"
-            }
-            ]
-        },
-        {
-            id:4,
-            firstName:"yuta",
-            lastName:"wild",
-            skills:[
-            {
-                id:2,
-                skillName:"javascript"
-            }
-            ]
-        },
-    ]
+
+    const [wilders, setWilders] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
+    useEffect(() => {
+        (async () => {
+            const response = await fetch ("/wilders");
+            setWilders(await response.json());
+            setIsLoading(false);
+        })();
+
+    }, [])
+
     return (
         <>
             <h2>Wilders</h2>
             <section className="card-row">
-            {
-                wilders.map((wilder) => 
-                <Wilder key={wilder.id} firstName={wilder.firstName} lastName={wilder.lastName} skills={wilder.skills}/>
-                )
-            }
+                {isLoading?
+                    <p> Loading... </p>
+                    
+                    :
+                    
+                    wilders?.map((wilder) => 
+                    <Wilder key={wilder.id} firstName={wilder.firstName} lastName={wilder.lastName} skills={wilder.skills} isTrainer={wilder.isTrainer}/>
+                    )
+                }
+                
             </section>
         </>
   )
